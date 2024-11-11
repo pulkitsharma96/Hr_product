ee32bc68788b75530a815c255eafa65275ea078c
Ashima-Dhawan <ashimadhawan017@gmail.com>
2024-11-11 20:06:11 +0530

    forgot screen

--- lib/forgot_pswd.dart
+++ lib/forgot_pswd.dart
@@ -2,8 +2,6 @@ import 'package:flutter/material.dart';
 import 'package:fluttertoast/fluttertoast.dart';
 import 'package:hr_product/login_Screen.dart';
 
-
-
 class ForgotPasswordScreen extends StatefulWidget {
   const ForgotPasswordScreen({super.key});
 
@@ -12,29 +10,25 @@ class ForgotPasswordScreen extends StatefulWidget {
 }
 
 class _ForgotPasswordScreenState extends State<ForgotPasswordScreen> {
-
-  bool isCheck= false;
+  bool isCheck = false;
   final TextEditingController passwordController = TextEditingController();
   final TextEditingController confirmpasswordController = TextEditingController();
 
   bool _validationForgotPassword() {
     if (passwordController.text.isEmpty) {
-      Fluttertoast.showToast(msg: "enter atleast 6 character");
+      Fluttertoast.showToast(msg: "Enter at least 6 characters");
       return false;
-    }
-    else if (confirmpasswordController.text.length < 6) {
-      Fluttertoast.showToast(msg: "passwords do not match ");
+    } else if (confirmpasswordController.text.length < 6) {
+      Fluttertoast.showToast(msg: "Passwords do not match");
       return false;
-    }
-    else {
-      Fluttertoast.showToast(msg: "Password change successfully!");
+    } else {
+      Fluttertoast.showToast(msg: "Password changed successfully!");
       return true;
     }
   }
 
   @override
   Widget build(BuildContext context) {
-
     double screenHeight = MediaQuery.of(context).size.height;
     double screenWidth = MediaQuery.of(context).size.width;
 
@@ -42,55 +36,71 @@ class _ForgotPasswordScreenState extends State<ForgotPasswordScreen> {
       backgroundColor: Colors.white,
       body: Center(
         child: LayoutBuilder(
-          builder: (BuildContext context, BoxConstraints constraints){
-            bool isMobile = constraints.maxWidth <800;
+          builder: (BuildContext context, BoxConstraints constraints) {
+            bool isMobile = constraints.maxWidth < 800;
 
             return SingleChildScrollView(
               child: Container(
-                  width: screenWidth,
-                  margin:  const EdgeInsets.all(27),
-
-                  decoration: BoxDecoration(
-                    borderRadius: BorderRadius.circular(4),
-                    color: Colors.white,
-                  ),
-                  child: isMobile
-                      ? Column(
-                    children: [
-                      Container(
-                        margin: const EdgeInsets.all(25),
-                        height: 150,
-                        width: screenWidth,
-                        decoration: const BoxDecoration(
-                          image: DecorationImage(image: AssetImage('assets/images/forgot_image.jpg'),
-
-                          ),
-                        ),
+                width: screenWidth,
+                margin: const EdgeInsets.all(27),
+                decoration: BoxDecoration(
+                  borderRadius: BorderRadius.circular(4),
+                  color: Colors.white,
+                ),
+                child: Column(
+                  children: [
+                    Align(
+                      alignment: Alignment.topLeft,
+                      child: IconButton(
+                        icon: const Icon(Icons.arrow_back, color: Colors.black),
+                        onPressed: () {
+                          Navigator.push(
+                              context,
+                              MaterialPageRoute(
+                                  builder: (context) => LoginScreen()));
+                        },
                       ),
-                      _ForgotPasswordForm(screenWidth,screenHeight),
-                    ],
-                  )
-                      : Row(
-                    children: [
-                      Expanded(
-                        flex: 1,
-                        child:
+                    ),
+                    isMobile
+                        ? Column(
+                      children: [
                         Container(
-                          height: screenHeight*0.72,
+                          margin: const EdgeInsets.all(25),
+                          height: 150,
+                          width: screenWidth,
                           decoration: const BoxDecoration(
-                            image: DecorationImage(image: AssetImage('assets/images/forgot_image.jpg')
+                            image: DecorationImage(
+                              image: AssetImage('assets/images/forgot_image.jpg'),
                             ),
                           ),
                         ),
-                      ),
-                      Expanded(
+                        _ForgotPasswordForm(screenWidth, screenHeight),
+                      ],
+                    )
+                        : Row(
+                      children: [
+                        Expanded(
+                          flex: 1,
+                          child: Container(
+                            height: screenHeight * 0.72,
+                            decoration: const BoxDecoration(
+                              image: DecorationImage(
+                                image: AssetImage('assets/images/forgot_image.jpg'),
+                              ),
+                            ),
+                          ),
+                        ),
+                        Expanded(
                           flex: 1,
                           child: Padding(
                             padding: const EdgeInsets.only(right: 80),
-                            child: _ForgotPasswordForm(screenWidth,screenHeight),
-                          )),
-                    ],
-                  )
+                            child: _ForgotPasswordForm(screenWidth, screenHeight),
+                          ),
+                        ),
+                      ],
+                    ),
+                  ],
+                ),
               ),
             );
           },
@@ -99,103 +109,103 @@ class _ForgotPasswordScreenState extends State<ForgotPasswordScreen> {
     );
   }
 
-  Widget _ForgotPasswordForm(double screenwidth,double screenheight){
-    return  Container(
-        padding: const EdgeInsets.all(18),
-        decoration: BoxDecoration(
-          color: Colors.white,
-          boxShadow: [
-            BoxShadow(
-              spreadRadius: 1,
-              blurRadius: 15,
-              color: Colors.grey.withOpacity(0.7),
-            )
-          ],
-          borderRadius: BorderRadius.circular(10),
-        ),
-
-        child:
-        Column(
-          mainAxisAlignment: MainAxisAlignment.start,
-          crossAxisAlignment: CrossAxisAlignment.start,
-          children: [
-            const Text("Forgot Password?",style: TextStyle(fontSize: 25,fontWeight: FontWeight.bold),),
-            const SizedBox(height: 20),
-            const Text("Password",style: TextStyle(fontWeight: FontWeight.bold)),
-            const SizedBox(height: 3),
-            Container(
-              height: 42,
-              child: TextFormField(
-                controller: passwordController,
-                cursorColor: Colors.grey,
-                decoration: InputDecoration(
-                  hintText: "Enter 6 characters or more",
-                  hintStyle: const TextStyle(color: Colors.grey,),
-                  contentPadding: const EdgeInsets.symmetric(horizontal: 16),
-                  border: OutlineInputBorder(
-                    borderRadius: BorderRadius.circular(7),
-                    borderSide: const BorderSide(color: Colors.grey),
-                  ),
-                  focusedBorder: OutlineInputBorder(
-                    borderRadius: BorderRadius.circular(7),
-                    borderSide: const BorderSide(color: Colors.grey),),
+  Widget _ForgotPasswordForm(double screenWidth, double screenHeight) {
+    return Container(
+      padding: const EdgeInsets.all(18),
+      decoration: BoxDecoration(
+        color: Colors.white,
+        boxShadow: [
+          BoxShadow(
+            spreadRadius: 1,
+            blurRadius: 15,
+            color: Colors.grey.withOpacity(0.7),
+          ),
+        ],
+        borderRadius: BorderRadius.circular(10),
+      ),
+      child: Column(
+        mainAxisAlignment: MainAxisAlignment.start,
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: [
+          const Text(
+            "Forgot Password?",
+            style: TextStyle(fontSize: 25, fontWeight: FontWeight.bold),
+          ),
+          const SizedBox(height: 20),
+          const Text("Password", style: TextStyle(fontWeight: FontWeight.bold)),
+          const SizedBox(height: 3),
+          SizedBox(
+            height: 42,
+            child: TextFormField(
+              controller: passwordController,
+              cursorColor: Colors.grey,
+              decoration: InputDecoration(
+                hintText: "Enter 6 characters or more",
+                hintStyle: const TextStyle(color: Colors.grey),
+                contentPadding: const EdgeInsets.symmetric(horizontal: 16),
+                border: OutlineInputBorder(
+                  borderRadius: BorderRadius.circular(7),
+                  borderSide: const BorderSide(color: Colors.grey),
+                ),
+                focusedBorder: OutlineInputBorder(
+                  borderRadius: BorderRadius.circular(7),
+                  borderSide: const BorderSide(color: Colors.grey),
                 ),
               ),
             ),
-            const SizedBox(height: 15,),
-            const Text("Confirm Password",style: TextStyle(fontWeight: FontWeight.bold)),
-            const SizedBox(height: 3),
-            Container(
-              height: 42,
-              child: TextFormField(
-                controller: confirmpasswordController,
-                cursorColor: Colors.grey,
-                decoration: InputDecoration(
-                  hintText: "Password must be same",
-                  hintStyle: const TextStyle(color: Colors.grey,),
-                  contentPadding: const EdgeInsets.symmetric(horizontal: 16),
-                  border: OutlineInputBorder(
-                    borderRadius: BorderRadius.circular(7),
-                    borderSide: const BorderSide(color: Colors.grey),
-                  ),
-                  focusedBorder: OutlineInputBorder(
-                    borderRadius: BorderRadius.circular(7),
-                    borderSide: const BorderSide(color: Colors.grey),),
+          ),
+          const SizedBox(height: 15),
+          const Text("Confirm Password", style: TextStyle(fontWeight: FontWeight.bold)),
+          const SizedBox(height: 3),
+          SizedBox(
+            height: 42,
+            child: TextFormField(
+              controller: confirmpasswordController,
+              cursorColor: Colors.grey,
+              decoration: InputDecoration(
+                hintText: "Password must be the same",
+                hintStyle: const TextStyle(color: Colors.grey),
+                contentPadding: const EdgeInsets.symmetric(horizontal: 16),
+                border: OutlineInputBorder(
+                  borderRadius: BorderRadius.circular(7),
+                  borderSide: const BorderSide(color: Colors.grey),
+                ),
+                focusedBorder: OutlineInputBorder(
+                  borderRadius: BorderRadius.circular(7),
+                  borderSide: const BorderSide(color: Colors.grey),
                 ),
               ),
             ),
-            const SizedBox(height: 25),
-            SizedBox(
-              height: 43,
-              width: screenwidth,
-              child: ElevatedButton(
-                  style: ElevatedButton.styleFrom(
-                    shape: RoundedRectangleBorder(
-                      borderRadius: BorderRadius.circular(7),
-                    ),
-                    backgroundColor: Colors.deepPurpleAccent,
-                    padding: const EdgeInsets.symmetric(horizontal: 16),
-                  ),
-
-                  onPressed: (){
-                    if(_validationForgotPassword()){
-                      Navigator.push(
-                          context,
-                          MaterialPageRoute(
-                              builder: (context) => LoginScreen()));
-                    }
-                  },
-                  child: const Text("Continue",style: TextStyle(color: Colors.white,fontWeight: FontWeight.bold,fontSize: 14),)),
+          ),
+          const SizedBox(height: 25),
+          SizedBox(
+            height: 43,
+            width: screenWidth,
+            child: ElevatedButton(
+              style: ElevatedButton.styleFrom(
+                shape: RoundedRectangleBorder(
+                  borderRadius: BorderRadius.circular(7),
+                ),
+                backgroundColor: Colors.deepPurpleAccent,
+                padding: const EdgeInsets.symmetric(horizontal: 16),
+              ),
+              onPressed: () {
+                if (_validationForgotPassword()) {
+                  Navigator.push(
+                    context,
+                    MaterialPageRoute(builder: (context) => LoginScreen()),
+                  );
+                }
+              },
+              child: const Text(
+                "Continue",
+                style: TextStyle(color: Colors.white, fontWeight: FontWeight.bold, fontSize: 14),
+              ),
             ),
-            const SizedBox(height: 15),
-          ],
-        )
+          ),
+          const SizedBox(height: 15),
+        ],
+      ),
     );
-
   }
 }
-
-
-
-
-
